98c1160cbae7860eb90471856f65e7762ae0d9f9
Manas <prometheansacrifice@gmail.com>
2019-05-28 19:00:38 +0530

    Build Skia using MinGW

--- esy/build.sh
+++ esy/build.sh
@@ -12,7 +12,7 @@ python tools/git-sync-deps
 ln -s third_party/externals/gyp tools/gyp
 if [[ $OS == 'windows' ]]
 then
-    bin/gn gen $cur__target_dir/out/Release --args='is_debug=false' || exit -1
+    bin/gn gen $cur__target_dir/out/Release --args="cc=\"x86_64-w64-mingw32-gcc.exe\" cxx=\"x86_64-w64-mingw32-g++.exe\" is_debug=false" || exit -1
 else
     bin/gn gen $cur__target_dir/out/Release "--args=cc=\"clang\" cxx=\"clang++\" skia_use_system_libjpeg_turbo=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
 fi
