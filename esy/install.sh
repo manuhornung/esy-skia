0025e45e959d7cdc8daa7e868be09bf2469d1547
Manuel Hornung <manuhornung@mun-mane.sto.netlight.se>
2019-08-18 13:55:05 +0200

    Remove C++ public API headers

--- esy/install.sh
+++ esy/install.sh
@@ -30,7 +30,7 @@ libdir=$cur__lib
 Name: skia
 Description: 2D graphics library
 Version: $cur__version
-Cflags: -I$cur__root -I\${includedir}/android -I\${includedir}/atlastext -I\${includedir}/c -I\${includedir}/codec -I\${includedir}/config -I\${includedir}/core -I\${includedir}/docs -I\${includedir}/effects -I\${includedir}/encode -I\${includedir}/gpu -I\${includedir}/pathops -I\${includedir}/ports -I\${includedir}/private -I\${includedir}/svg -I\${includedir}/third_party -I\${includedir}/utils -std=c++1y
+Cflags: -I$cur__root -I\${includedir}/c -std=c++1y
 Libs: -L\${libdir} $platformSpecificFlags -lskia -lstdc++
 EOF
 
