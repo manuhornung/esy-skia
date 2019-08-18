5ebf7993bac322fd4e41494679be840ded5e35ca
Manuel Hornung <manuhornung@mun-mane.sto.netlight.se>
2019-08-18 10:44:43 +0200

    Use the correct libskia.a name instead of skia.a

--- esy/build.sh
+++ esy/build.sh
@@ -14,7 +14,7 @@ then
     bin/gn gen $cur__target_dir/out/Shared --args='is_debug=false is_component_build=true'
     ninja.exe -C $cur__target_dir/out/Shared
     esy/gendef.exe - $cur__target_dir/out/Shared/skia.dll > $cur__target_dir/out/Shared/skia.def
-    x86_64-W64-mingw32-dlltool.exe -D $cur__target_dir/out/Shared/skia.dll -d $cur__target_dir/out/Shared/skia.def -A -l $cur__target_dir/out/Shared/skia.a
+    x86_64-W64-mingw32-dlltool.exe -D $cur__target_dir/out/Shared/skia.dll -d $cur__target_dir/out/Shared/skia.def -A -l $cur__target_dir/out/Shared/libskia.a
 else
     bin/gn gen $cur__target_dir/out/Static "--args=cc=\"clang\" cxx=\"clang++\" skia_use_system_libjpeg_turbo=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
     ninja.exe -C $cur__target_dir/out/Static
--- esy/install.sh
+++ esy/install.sh
@@ -5,10 +5,10 @@ OS=$1
 # Copy artifacts into output directories
 if [[ $OS == 'windows' ]]
 then
-    cp -a $cur__target_dir/out/Shared/skia.a $cur__lib
+    cp -a $cur__target_dir/out/Shared/libskia.a $cur__lib
     cp -a $cur__target_dir/out/Shared/skia.dll $cur__bin
 else
-    cp -a $cur__target_dir/out/Static/skia.a $cur__lib
+    cp -a $cur__target_dir/out/Static/libskia.a $cur__lib
 fi
 
 # Create pkg-config file skia.pc
@@ -30,7 +30,7 @@ libdir=$cur__lib
 Name: skia
 Description: 2D graphics library
 Version: $cur__version
-Cflags: -I\${includedir}/android -I\${includedir}/atlastext -I\${includedir}/c -I\${includedir}/codec -I\${includedir}/config -I\${includedir}/core -I\${includedir}/docs -I\${includedir}/effects -I\${includedir}/encode -I\${includedir}/gpu -I\${includedir}/pathops -I\${includedir}/ports -I\${includedir}/private -I\${includedir}/svg -I\${includedir}/third_party -I\${includedir}/utils -std=c++1y
+Cflags: -I$cur__root -I\${includedir}/android -I\${includedir}/atlastext -I\${includedir}/c -I\${includedir}/codec -I\${includedir}/config -I\${includedir}/core -I\${includedir}/docs -I\${includedir}/effects -I\${includedir}/encode -I\${includedir}/gpu -I\${includedir}/pathops -I\${includedir}/ports -I\${includedir}/private -I\${includedir}/svg -I\${includedir}/third_party -I\${includedir}/utils -std=c++1y
 Libs: -L\${libdir} $platformSpecificFlags -lskia -lstdc++
 EOF
 
